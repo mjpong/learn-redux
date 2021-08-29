60ed18122626ec23a43b867f6b18e24d2a137369
mjpong <jolenepong@gmail.com>
2021-08-29 19:10:35 +0800

    post + form to test api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import './App.css';
 import React from "react";
 import { useSelector, useDispatch } from 'react-redux';
-import { increment, decrement } from './actions';
+// import { increment, decrement } from './actions';
+import Posts from "./components/Posts"
+import Postform from './components/Postform';
+
+import allReducers from "./reducers"
+import { Provider } from 'react-redux';
+import store from "./components/store"
+
+
+// const store = createStore(
+//   allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// );
+
+
+
 
 function App() {
 
-  const dispatch = useDispatch();
-  const counter = useSelector(state => state.counter);
-  const isLogged = useSelector(state => state.isLogged);
+  // const dispatch = useDispatch();
+  // const counter = useSelector(state => state.counter);
+  // const isLogged = useSelector(state => state.isLogged);
 
 
 
   return (
-    <div className="App">
-      <h1>Counter {counter} </h1>
-      <button onClick={() => dispatch(increment(10))}>+</button>
-      <button onClick={() => dispatch(decrement(5))}>-</button>
-      {isLogged ? <h3>Valuable Info</h3> : ''}
 
-    </div>
+    // <div className="counter-test">
+    //   <h1>Counter {counter} </h1>
+    //   <button onClick={() => dispatch(increment(10))}>+</button>
+    //   <button onClick={() => dispatch(decrement(5))}>-</button>
+    //   {isLogged ? <h3>Valuable Info</h3> : ''}
+    // </div>
+    <Provider store={store}>
+      <Postform />
+      <hr />
+      <Posts />
+    </Provider>
+
   );
 }
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,18 @@
-import counterReducer from "./counter";
-import loggedReducer from "./isLogged";
-import { combineReducers } from 'redux';
+// import counterReducer from "./counter";
+// import loggedReducer from "./isLogged";
+// import { combineReducers } from 'redux';
 
-const allReducers = combineReducers({
-    counter: counterReducer,
-    isLogged: loggedReducer
+// const allReducers = combineReducers({
+//     counter: counterReducer,
+//     isLogged: loggedReducer
+// })
+
+// export default allReducers;
+
+
+import { combineReducers } from "redux";
+import postReducer from "./postReducer"
+
+export default combineReducers({
+    posts: postReducer
 })
-
-export default allReducers;
